e42b7522d136e45dd6ee05b6e3dae5ea4a4b807d
user <user@DESKTOP-F4T27T3>
2020-12-15 13:31:33 +0900

    placeholder 추가 및 Controller 소스수정

--- musinsa-khg-1/src/main/java/com/example/demo/UrlShorteningController.java
+++ musinsa-khg-1/src/main/java/com/example/demo/UrlShorteningController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.demo.service.UrlShorteningService;
 import com.example.demo.service.UrlShorteningVO;
@@ -39,13 +40,17 @@ public class UrlShorteningController {
 	// 중복 된 originalUrl이 있는지 확인용
 	private Map<String, UrlShorteningVO> ORIGINAL_URL_KEY_MAP = new HashMap<String, UrlShorteningVO>();
 	
-	@RequestMapping(value = "/")
+	private static final String ORIGINAL_URL = "originalUrl";
+	private static final String HTTP = "http://";
+	private static final String HTTPS = "https://";
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String urlShorteningRegisterForm() {
 		return "urlShorteningRegister";
 	}
 	
 	@RequestMapping(value = "/{shortUrl}")
-	public String moveShorteningUrl(@PathVariable("shortUrl") String shortUrl) {
+	public String moveShorteningUrl(@PathVariable(required = true) String shortUrl) {
 
 		// 짧은 URL로 등록 된 데이터가 없다면 없는 화면으로
 		if (!SHORT_URL_KEY_MAP.containsKey(shortUrl)) {
@@ -61,11 +66,11 @@ public class UrlShorteningController {
 	@RequestMapping(value = "/addOriginalUrl")
 	public String addOriginalUrl(HttpServletRequest request, Model model) {
 		
-		String originalUrl = request.getParameter("originalUrl");
+		String originalUrl = request.getParameter(ORIGINAL_URL);
 		
 		// originalUrl http가 없으면 앞에 생성
-		if (!originalUrl.contains("http")) {
-			originalUrl = "http://".concat(originalUrl);
+		if (!originalUrl.startsWith(HTTP) && !originalUrl.startsWith(HTTPS)) {
+			originalUrl = HTTP.concat(originalUrl);
 		}
 
 		UrlShorteningVO resultData = urlShorteningService.getUrlShorteningData(originalUrl, ORIGINAL_URL_KEY_MAP, SHORT_URL_KEY_MAP);
